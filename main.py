aaa73236fa7ba0b99f48f2c47575c32fd09be20c
Tortik-tortivich <zhovtonohartem03@gmail.com>
2025-01-12 16:47:00 +0200

    main.py

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ keyboard.add_hotkey('Alt + Z',set_clicker)
 while True:
     if isClicking:
         #Если вам надо чтобы нажимала кнопку на клавиатуре,то пропишите keyboard.press_and_release('Название кнопки')/If you need to press a button on the keyboard, then write keyboard.press_and_release('Button name').
-        mouse.press(button='left')
+        mouse.double_click(button='left')
         #Вы можете поставить любое время на нажатие/You can set any time to press.
         time.sleep(0.01)
